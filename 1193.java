50b0c8f0f830c85a6c8f54b7d35d89d2c9b6fd56
cmong0516 <cmong0516@gmail.com>
2022-05-20 16:19:35 +0900

    2022.5.20

---
+++ 1193.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.solution();
+    }
+
+    public int solution() {
+        int answer = 0;
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        return answer;
+    }
+}
