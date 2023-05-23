d6748c1ca22839eaff281deaa9d4513dd979465d
cmong0516 <cmong0516@gmail.com>
2023-05-23 13:15:39 +0900

    2023.05.23

--- BaekJoon2110.java
+++ BaekJoon2110.java
@@ -2,13 +2,16 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BaekJoon2110 {
+
+    static int[] arr;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int c = sc.nextInt();
 
-        int[] arr = new int[n];
+        arr = new int[n];
 
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
@@ -22,5 +25,39 @@ public class BaekJoon2110 {
 
         // 거리가 늘어난다 = 공유기 갯수가 준다.
         // 거리가 줄어든다 = 공유기 갯수가 는다.
+
+        int low = 1;
+        int high = arr[n-1] - arr[0]+1;
+
+        while (low < high) {
+            System.out.println("low = " + low);
+            System.out.println("high = " + high);
+
+            int mid = (low + high) /2;
+            if (install(mid) < c) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+
+        System.out.println(low-1);
+    }
+
+    public static int install(int distance) {
+        int count = 1;
+        int lastLocation = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            int location = arr[i];
+
+            if (location - lastLocation >= distance) {
+                count++;
+                lastLocation = location;
+            }
+        }
+
+        return count;
     }
 }
