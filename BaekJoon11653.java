f1dd2f369cfbbff8321316363d6cb9de1dbcebb9
cmong0516 <cmong0516@gmail.com>
2023-05-27 05:27:41 +0900

    2023.05.27

---
+++ BaekJoon11653.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BaekJoon11653 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 2; i < n; i++) {
+            System.out.println("i = " + i);
+            System.out.println("n = " + n);
+            if (n % i == 0) {
+                n /= i;
+                list.add(i);
+                i = 2;
+            }
+        }
+
+        System.out.println("list = " + list);
+    }
+}
