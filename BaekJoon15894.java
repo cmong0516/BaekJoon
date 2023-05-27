7374f0066c84f30904cbf73fe7b98cbd9668ebb2
cmong0516 <cmong0516@gmail.com>
2023-05-28 00:42:23 +0900

    2023.05.28

---
+++ BaekJoon15894.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BaekJoon15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        // 1 1개 4
+        // 2 3개 8
+        // 3 6개 12
+
+        System.out.println(n*4);
+    }
+}
