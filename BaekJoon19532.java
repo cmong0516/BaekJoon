0eaa1d44e2800386a24e3810f73609225ca06775
cmong0516 <cmong0516@gmail.com>
2023-06-09 05:47:04 +0900

    2023.06.09

---
+++ BaekJoon19532.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BaekJoon19532 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+
+        // ax + by = c
+        // dx + ey = f
+
+        for (int i = -999; i < 1000; i++) {
+            for (int j = -999; j < 1000; j++) {
+                if (a * i + b * j == c && d * i + e * j == f) {
+                    System.out.println(i + " " + j);
+                }
+            }
+        }
+    }
+}
