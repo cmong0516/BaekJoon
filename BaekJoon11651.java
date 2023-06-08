b35e5aaf0db45131117a08a6110ff764ed3a3356
cmong0516 <cmong0516@gmail.com>
2023-06-09 02:54:00 +0900

    2023.06.09

--- BaekJoon11651.java
+++ BaekJoon11651.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class BaekJoon11650 {
+public class BaekJoon11651 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -15,16 +15,15 @@ public class BaekJoon11650 {
         }
 
         Arrays.sort(arr,(i1,i2) -> {
-            if (i1[0] == i2[0]) {
-                return i1[1] - i2[1];
-            } else {
+            if (i1[1] == i2[1]) {
                 return i1[0] - i2[0];
+            } else {
+                return i1[1] - i2[1];
             }
         });
 
         for (int i = 0; i < n; i++) {
             System.out.println(arr[i][0] + " " + arr[i][1]);
         }
-
     }
 }
