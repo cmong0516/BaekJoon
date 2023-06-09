420bf51ce2878065538fcbcae3b59bc514bb5e1c
cmong0516 <cmong0516@gmail.com>
2023-06-10 05:11:56 +0900

    2023.06.10

---
+++ BaekJoon2563.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BaekJoon2563 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] arr = new int[100][100];
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            for (int j = x; j < x+10; j++) {
+                for (int k = y; k < y+10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j] == 1) {
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
