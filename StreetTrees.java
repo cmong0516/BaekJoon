111c653f93ceb935b2b26806705e6827e6c3acc1
cmong0516 <cmong0516@gmail.com>
2023-07-31 00:09:22 +0900

    2023.07.31

--- StreetTrees.java
+++ StreetTrees.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class StreetTrees {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            list.add(sc.nextInt());
+        }
+
+        Collections.sort(list);
+
+        System.out.println("list = " + list);
     }
 }
