232067748341b30f36b63948e4fb530b6474bb75
cmong0516 <cmong0516@gmail.com>
2023-08-21 17:26:31 +0900

    2023.08.21

--- BaekJoon1929.java
+++ BaekJoon1929.java
@@ -14,6 +14,11 @@ public class BaekJoon1929 {
     }
 
     public static boolean isPrime(int n) {
+
+        if (n == 1) {
+            return false;
+        }
+
         for (int i = 2; i<=(int)Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
