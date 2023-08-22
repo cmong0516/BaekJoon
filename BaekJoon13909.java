576cc8d8908202632b001828376a49cc5bf901b7
cmong0516 <cmong0516@gmail.com>
2023-08-22 22:36:14 +0900

    2023.08.22

--- BaekJoon13909.java
+++ BaekJoon13909.java
@@ -5,27 +5,12 @@ public class BaekJoon13909 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        boolean[] arr = new boolean[n];
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i % j == 0) {
-                    arr[i-1] = !arr[i-1];
-                }
-            }
-        }
-
         int count = 0;
 
-        for (boolean b : arr) {
-            if (b == true) {
-                count++;
-            }
+        for (int i = 1; i*i < n; i++) {
+            count++;
         }
 
         System.out.println(count);
     }
 }
-
-// 메모리 초과
-// 21억 크기의 배열을 만들어야한다...
