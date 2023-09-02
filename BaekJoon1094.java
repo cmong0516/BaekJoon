423aaa2af1ccf0e46677c94f6bff8edc43ffabf5
cmong0516 <cmong0516@gmail.com>
2023-09-03 02:15:49 +0900

    2023.09.03C

--- BaekJoon1094.java
+++ BaekJoon1094.java
@@ -1,42 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class BaekJoon1094 {
-
-    static List<Integer> list;
-
     public static void main(String[] args) {
-        list = new ArrayList<>();
-        list.add(64);
-    }
+        Scanner sc = new Scanner(System.in);
 
-    public static int cut(int x) {
+        int X = sc.nextInt();
+        int stick = 64;
+        int count = 0;
 
-        // 가지고 있는 막대중 가장 짧은것
-        sortList();
-        Integer theSmallestStick = list.get(0);
-        // 반으로 자른다.
-        int halfSmallesStick = theSmallestStick / 2;
-        // 반으로 잘린 막대는 이제 없음
-        list.remove(theSmallestStick);
-        // 가장 작은 막대를 반으로 잘라서 생긴 두개의 막대
-        list.add(halfSmallesStick);
-        list.add(halfSmallesStick);
-
-        // 위에서 자른 막대의 절반중 하나를 버리고 남아있는 막대의 길이의 합이
-        list.remove(halfSmallesStick);
-
-        int total = list.stream().mapToInt(Integer::intValue).sum();
-        // x 보다 크거나 같으면
-        if (total >= x) {
-
+        while (X > 0) {
+            if (stick > X) {
+                stick /= 2;
+            } else {
+                X -= stick;
+                count++;
+            }
         }
 
-
-        return 1;
-    }
-
-    public static void sortList() {
-        list.sort((o1, o2) -> o1-o2);
+        System.out.println(count);
     }
 }
