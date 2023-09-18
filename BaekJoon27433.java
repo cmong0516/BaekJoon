02b4db89c4486c4123257f742e76b84afebe3e43
cmong0516 <cmong0516@gmail.com>
2023-09-19 07:29:39 +0900

    2023.09.19

---
+++ BaekJoon27433.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon27433 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = 1;
+
+        for (int i = 1; i <= N; i++) {
+            answer *= i;
+        }
+
+        System.out.println(answer);
+    }
+}
