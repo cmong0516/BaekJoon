74ac2ab8f78adde1519e7b96b0e33677594c5415
cmong0516 <cmong0516@gmail.com>
2023-09-18 17:44:32 +0900

    2023.09.18

---
+++ BaekJoon2108.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon2108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[8001];
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        int median = 10000;
+        int mode = 10000;
+
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(br.readLine());
+            sum += value;
+            arr[value + 4000]++;
+
+            if (max < value) {
+                max = value;
+            }
+
+            if (min > value) {
+                min = value;
+            }
+        }
+
+        int count = 0;
+        int mode_max = 0;
+
+        boolean flag = false;
+
+        for (int i = min + 4000; i <= max + 4000; i++) {
+
+            // arr[i] 가 양수일때
+            if (arr[i] > 0) {
+
+                if (count < (N + 1) / 2) {
+                    count += arr[i];
+                    median = i - 4000;
+                }
+
+                if (mode_max < arr[i]) {
+                    mode_max = arr[i];
+                    mode = i - 4000;
+                    flag = true;
+                }
+
+
+            } else if (mode_max == arr[i] && flag == true) {
+                mode = i - 4000;
+                flag = false;
+            }
+        }
+    }
+}
