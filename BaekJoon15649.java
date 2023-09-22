5cfc14aebb38ed3b4af69f0c2df203294f76b62c
cmong0516 <cmong0516@gmail.com>
2023-09-22 09:31:08 +0900

    2023.09.22

--- BaekJoon15649.java
+++ BaekJoon15649.java
@@ -1,5 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class BaekJoon15649 {
-    public static void main(String[] args) {
+
+    static int[]arr;
+    static boolean[]visit;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+
+        dfs(N, M, 0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i +1;
+                // i = 0;
+                // arr[0] = 1;
+                // depth == M
+                // sb.append 1
+
+                // i = 1;
+                // arr[0] = 2;
+                // depth == M
+                // sb.append 2
+                dfs(N,M,depth+1);
+                visit[i] = false;
+            }
+
+        }
+
+
+
 
     }
 }
