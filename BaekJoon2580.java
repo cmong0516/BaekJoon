ee733b9ba673fafde4ec3cbf6fa0a3156ac62104
cmong0516 <cmong0516@gmail.com>
2023-09-24 19:27:04 +0900

    2023.09.24

---
+++ BaekJoon2580.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon2580 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
