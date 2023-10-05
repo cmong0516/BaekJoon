8d08bb8b0e0b2d8c0de44f211dac3ccfa16ad3e4
cmong0516 <cmong0516@gmail.com>
2023-10-05 14:46:00 +0900

    2023.10.05

---
+++ BaekJoon1463.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon1463 {
+
+    static Integer[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        dp = new Integer[N+1];
+        dp[0] = dp[1] = 0;
+
+        System.out.println(solve(N));
+    }
+
+    public static int solve(int N) {
+        if (dp[N] == null) {
+            if (N % 6 == 0) {
+                dp[N] = Math.min(solve(N - 1), Math.min(solve(N / 3), solve(N / 2))) + 1;
+            }else if (N % 3 == 0) {
+                dp[N] = Math.min(solve(N / 3), solve(N - 1)) + 1;
+            }
+            // 2로만 나눠지는 경우
+            else if (N % 2 == 0) {
+                dp[N] = Math.min(solve(N / 2), solve(N - 1)) + 1;
+            }
+            // 2와 3으로 나누어지지 않는 경우
+            else {
+                dp[N] = solve(N - 1) + 1;
+            }
+        }
+
+        return dp[N];
+    }
+}
