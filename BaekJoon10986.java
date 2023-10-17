52c423d0edeb33d96523ca998094dc9a9d36fdea
cmong0516 <cmong0516@gmail.com>
2023-10-17 14:23:30 +0900

    2023.10.17

---
+++ BaekJoon10986.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon10986 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        long[] arr = new long[N + 1];
+
+        arr[0] = 0;
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
