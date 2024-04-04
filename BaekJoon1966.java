25ff977ab86106841bd3570b8456cfeacb1bd74b
cmong0516 <cmong0516@gmail.com>
2024-04-05 02:49:17 +0900

    2024.04.05

--- BaekJoon1966.java
+++ BaekJoon1966.java
@@ -8,16 +8,49 @@ public class BaekJoon1966 {
     public static void main(String[] args) {
         int t = sc.nextInt();
 
-        for (int i = 0; i < t; i++) {
+        while (t-- > 0) {
+
             int N = sc.nextInt();
             int M = sc.nextInt();
 
+            LinkedList<int[]> q = new LinkedList<>();
+
+            for (int i = 0; i < N; i++) {
+                q.offer(new int[] { i, sc.nextInt() });
+            }
+
+            int count = 0;
+
+            while (!q.isEmpty()) {
+
+                int[] front = q.poll();
+                boolean isMax = true;
+
+                for(int i = 0; i < q.size(); i++) {
+                    if(front[1] < q.get(i)[1]) {
+                        q.offer(front);
+                        for(int j = 0; j < i; j++) {
+                            q.offer(q.poll());
+                        }
+                        isMax = false;
+                        break;
+                    }
+                }
+
+                if(isMax == false) {
+                    continue;
+                }
+
+                count++;
+                if(front[0] == M) {
+                    break;
+                }
+
+            }
+
+            sb.append(count).append('\n');
 
         }
-    }
-
-    public static int solution(int N, int M, List<Integer> list) {
-        Queue<Integer> queue = new LinkedList<>();
-
+        System.out.println(sb);
     }
 }
